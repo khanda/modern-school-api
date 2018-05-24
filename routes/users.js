d74a115551b77f96b1adc15952a84d6dad77ebcb
quyendoan <doanquangquyen@gmail.com>
2018-05-24 16:25:04 +0700

    Add more lib

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+/* GET users listing. */
+router.get('/save', function(req, res, next) {
+    res.send('Save user');
+});
 module.exports = router;
